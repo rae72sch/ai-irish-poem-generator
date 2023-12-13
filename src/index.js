4c10f63fc82ef126f5d44690932db46fe5604787
Rachel Schofield <rae72sch@hotmail.com>
2023-12-13 13:19:43 +0000

    Basic form handling & added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// alert("Failte / Welcome!");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Placeholder for first line <br /> Placeholder for second line <br /> Placeholder for third line <br /> Placeholder for fourth line",
+    autoStart: true,
+    cursor: "",
+    delay: 10
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
