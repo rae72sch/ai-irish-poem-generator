88045c79ada508ca35f24a436471721a5f0f7800
Rachel Schofield <rae72sch@hotmail.com>
2023-12-16 13:32:24 +0000

    Attempting to fix the AI line spacing issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function generatePoem(event) {
   let userInput = document.querySelector("#user-input");
 
   let context =
-    "You are a poem expert and love to write short poems. Please return a four line poem in the Irish language. Please also include a translation of the poem below it. Please return all of this in basic HTML with a <br> after each line. Sign the poem by 'AI Poem Generator' at the end of the translation and make only the signature bold. Please leave a space between the poem and the translation. Don't give the poem a title.";
+    "You are a poem expert and love to write short poems. Please return a four line poem in the Irish language. Please also include a translation of the poem below it. Please return all of this in basic HTML. Sign the poem by 'AI Poem Generator' at the end of the translation and make only the signature bold. Please leave a space between the poem and the translation. Don't give the poem a title.";
   let prompt = `User instructions: Generate an Irish poem about ${userInput.value}`;
 
   let apiKey = "33840b8e1tc5oa820f7a06b487319f0d";
@@ -14,8 +14,9 @@ function generatePoem(event) {
 }
 
 function displayPoem(response) {
+  const responseFormatted = response.data.answer.replaceAll("\n", "<br />")
    new Typewriter("#poem", {
-     strings: response.data.answer,
+     strings: responseFormatted,
      autoStart: true,
      cursor: "",
      delay: 10,
