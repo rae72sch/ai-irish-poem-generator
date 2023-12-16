e423a221e715721503e378cd2a1ef1044f1e2f61
Rachel Schofield <rae72sch@hotmail.com>
2023-12-16 13:40:15 +0000

    Trying to solve the line spacing issues

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function generatePoem(event) {
   let userInput = document.querySelector("#user-input");
 
   let context =
-    "You are a poem expert and love to write short poems. Please return a four line poem in the Irish language. Please also include a translation of the poem below it. Please return all of this in basic HTML. Sign the poem by 'AI Poem Generator' at the end of the translation and make only the signature bold. Please leave a space between the poem and the translation. Don't give the poem a title.";
+    "You are a poem expert and love to write short poems. Please return a four line poem in the Irish language. Please also include a translation of the poem below it. Please return all of this in basic HTML. Sign the poem by 'AI Poem Generator' at the end of the translation and make only the signature bold. Please leave a space between the poem and the translation. Don't give the poem a title. Please don't make each line a new paragraph.";
   let prompt = `User instructions: Generate an Irish poem about ${userInput.value}`;
 
   let apiKey = "33840b8e1tc5oa820f7a06b487319f0d";
